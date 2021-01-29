b63e2b9981c74960f773bffd83c4cf983c571f81
Anurag Ajwani <anurag.ajwani@gmail.com>
2021-01-29 00:33:01 +0000

    Added sroll view with contents

--- PaellaRecipe/ViewController.swift
+++ PaellaRecipe/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 class ViewController: UIViewController {
     
     private let paellaImage = UIImage(named: "paella.JPG")!
+    
+    private var scrollView: UIScrollView!
+    private var scrollContentView: UIView!
 
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -26,7 +29,60 @@ class ViewController: UIViewController {
     }
     
     private func loadViews() {
+        self.loadScrollView()
+        self.loadContents()
+    }
+    
+    private func loadScrollView() {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.alignSubview(scrollView)
+        self.scrollView = scrollView
         
+        let contentView = UIView()
+        scrollView.alignSubview(contentView)
+        self.scrollContentView = contentView
+            
+        NSLayoutConstraint.activate([
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.heightAnchor.constraint(greaterThanOrEqualTo: scrollView.heightAnchor),
+        ])
+    }
+    
+    private func loadContents() {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        self.scrollView.alignSubview(stackView, withMargin: 15)
+        
+        let imageView = self.loadImageView()
+        stackView.addArrangedSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: self.paellaImage.size.height / self.paellaImage.size.width)
+        ])
+        stackView.addArrangedSubview(self.loadHeaderView())
+        stackView.addArrangedSubview(self.loadStepsView())
+    }
+    
+    private func loadImageView() -> UIImageView {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = self.paellaImage
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }
+        
+    private func loadHeaderView() -> RecipeHeaderView {
+        let headerView = RecipeHeaderView()
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        return headerView
+    }
+        
+    private func loadStepsView() -> RecipeStepsView {
+        let stepsView = RecipeStepsView()
+        stepsView.translatesAutoresizingMaskIntoConstraints = false
+        return stepsView
     }
 }
 
